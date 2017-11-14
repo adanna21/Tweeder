01ed0ce651cbd6fc437df867354279b4dc9f82b9
J Silverstein <jess.pebble@gmail.com>
2017-11-14 13:29:45 -0500

    fix server port

--- tweedr/server.js
+++ tweedr/server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 /* setting up port & listen */
-const PORT = process.env.PORT || 3021;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, function() {
   console.log(`listening on port ${PORT}`);
 });
