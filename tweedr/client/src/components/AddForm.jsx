f640d961c33c43a1846b33233c1da20a20a60f1b
Adanna Omatu <adaomatu@gmail.com>
2017-11-14 21:23:44 -0500

    added components and files

---
+++ tweedr/client/src/components/AddForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function AddForm (props) {
+  return (
+    <form>
+      <input type="text" />
+      <button type="submit">POST</button>
+    </form>
+  )
+}
+
+
+export default AddForm;
